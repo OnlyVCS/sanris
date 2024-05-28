e9c6b1c8f63a22c73e9516d818a68af43cded176
OnlyVCS <vcsdevacc@gmail.com>
2024-05-28 10:44:19 -0300

    next.config.jms changed

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,21 @@
 
 // export default nextConfig;
 
-import 'dotenv/config';
-
 export default {
+  output: 'export',
   reactStrictMode: true,
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    NEXT_PUBLIC_AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    NEXT_PUBLIC_DATABASE_URL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+    NEXT_PUBLIC_STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    NEXT_PUBLIC_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_APP_ID: process.env.NEXT_PUBLIC_APP_ID,
+    NEXT_PUBLIC_APP_ID_OPENWEATHER: process.env.NEXT_PUBLIC_APP_ID_OPENWEATHER
+  },
 };
